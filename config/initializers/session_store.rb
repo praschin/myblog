011775173a135c71a75e16854316f8c25e4cac63
Prasanth Chinta <chintaprasanth@gmail.com>
2014-07-13 23:40:04 -0700

    production.rb change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-MyBlog.application.config.session_store :cookie_store, key: '_myblog_session'
+Myblog.application.config.session_store :cookie_store, key: '_myblog_session'
