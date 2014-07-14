aa7dc53fed9fc0bd22cde91116386998b76d0482
Prasanth Chinta <chintaprasanth@gmail.com>
2014-07-13 22:42:34 -0700

    Changed Gemfile

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates_presence_of :post_id
+  validates_numericality_of :post_id, :only_integar => true
+  validates_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  validates_presence_of :Title
+  validates_presence_of :Body
 end 
