5cf5b97bf79be289f5def92b6989996c8125b7a0
Prasanth Chinta <chintaprasanth@gmail.com>
2014-07-13 23:37:06 -0700

    development.rb change1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-MyBlog.application.configure do
+Myblog.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
